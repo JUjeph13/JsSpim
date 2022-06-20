a3c8014f43464f8ced263fb77fb673f9c3b4e6f9
JUjeph13 <104795268+JUjeph13@users.noreply.github.com>
2022-06-20 01:38:38 -0400

    Update fibonacci.s

--- Tests/fibonacci.s
+++ Tests/fibonacci.s
@@ -50,6 +50,7 @@ fib:
 # Compute and return fibonacci number
 beqz $a0, zero      # if n == 0 return 0
 beq $a0, 1, one     # if n == 1 return 1
+beq $a0, 2, two     # if n == 2 return 2
 
 # Calling fib(n-1)
 sub $sp, $sp, 4     # storing return address
@@ -70,9 +71,24 @@ sw $v0, 0($sp)
 sub $sp, $sp, 4     # storing return address
 sw $ra, 0($sp)
 
-sub $a0, $a0, 2     # n = n-2
-jal fib             # call fib(n-2)
+sub $a0, $a0, 2     # n-2
+jal fib             # fib(n-2)
 add $a0, $a0, 2
+
+lw $ra, 0($sp)      # restoring return address
+add $sp, $sp, 4
+
+
+sub $sp, $sp, 4     # Push return value
+sw $v1, 0($sp)
+
+# Calling fib(n-3)
+sub $sp, $sp, 4     # storing return address
+sw $ra, 0($sp)
+
+sub $a0, $a0, 3     # n = n-3
+jal fib             # call fib(n-3)
+add $a0, $a0, 3
 
 lw $ra, 0($sp)      # restoring return address
 add $sp, $sp, 4
@@ -80,7 +96,8 @@ add $sp, $sp, 4
 lw $s7, 0($sp)      # Pop return value
 add $sp, $sp, 4
 
-add $v0, $v0, $s7   # f(n - 2)+fib(n-1)
+add $v0, $v0, $v1   # f( n - 2)+fib(n-1)
+add $v0, $v0, $s7   # f(n - 2)+fib(n-1)+ f(n-3)
 
 jr $ra              # decrement/next in stack
 
