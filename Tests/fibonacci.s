07b786a0f78d4ad50b6f83f44d9b66aa56dfb7a7
JUjeph13 <104795268+JUjeph13@users.noreply.github.com>
2022-06-20 03:10:46 -0400

    Update fibonacci.s

--- Tests/fibonacci.s
+++ Tests/fibonacci.s
@@ -1,99 +1,131 @@
-.text
+ .text
 main:
-la $a0, prompt
-li $v0, 4           # print
-syscall
+        li   $a0, 10
+        jal  fib      #Calling fibonacci through recursion      
+        
+        move    $a0, $v0    # from n to $a0            
+        li      $v0, 1      # result
+        syscall
 
-li $v0, 5           # read int
-syscall
+        li       $v0, 4   
+        syscall
 
-move $t4, $v0       # $t4 <- n
-addi $t4, $t4, 1
+        li      $a0, 1               # $a0 must be equal to 1
+        li      $a1, 0               # $a1 must be equal to 0
+        li      $a2, 10              # $a2 equal to n
+        jal     fib                  # Calling fibonacci
+        
+        move    $a0, $v0                
+        li      $v0, 1               # result
+        syscall
 
-li $t2, 1           # $t2 is an index i
+        li       $v0, 4   
+        syscall
 
-loop:
-move $a0, $t2
-move $v0, $t2
-jal fib             # call fib (n)
-move $t3, $v0       # result is in $t3
+        
+        li      $v0, 4     
+        la      $a0, exit_str        # print: "Enter to exit"
+        syscall
 
-# Output message and n
-la $a0, result      # Print F_
-li $v0, 4
-syscall
+        li      $v0, 8       
+        la      $a0, new_str         # Wait for enter to exit
+        li      $a1, 32     
+        syscall
 
-move $a0, $t2       # Print n
-li $v0, 1
-syscall
 
-la $a0, result2     # Print =
-li $v0, 4
-syscall
+exit_main:
+        lw      $ra, 12($sp)         # Restore return address
+        lw      $fp, 8($sp)          # Restore frame pointer
+        addiu   $sp, $sp, 24         # Pop stack frame
+        
+        jr      $ra    
 
-move $a0, $t3       # Print the answer
-li $v0, 1
-syscall
+#int fib(int n)
+#{
+#       if (n == 0)
+#               return 0;
+#       else if (n == 1)
+#               return 1;
+#       else
+#               return fib(n - 1) + fib(n - 2);
+#}
+#
+#
+# a0 = n, we use s0
 
-la $a0, endl        # Print '\n'
-li $v0, 4
-syscall
-
-addi $t2, $t2, 1    # i++
-bne $t2, $t4, loop
-
-# End program
-li $v0, 10
-syscall
 
 fib:
-# Compute and return fibonacci number
-beqz $a0, zero      # if n == 0 return 0
-beq $a0, 1, one     # if n == 1 return 1
+        subu    $sp, $sp, 32         # Allocate stack frame of 32 btyes
+        sw      $ra, 20($sp)         # Save return address
+        sw      $fp, 16($sp)         # Save frame pointer
+        sw      $s0, 12($sp)         # Save s0 
+        addiu   $fp, $sp, 28         # Setup stack frame
 
-# Calling fib(n-1)
-sub $sp, $sp, 4     # storing return address
-sw $ra, 0($sp)
+# if (n == 0) then return 0
+B1_fib:
+        seq     $t0, $a0, $zero
+        beq     $t0, $zero, B2_fib   # if (n != 0) ==> goto B2
+        li      $v0, 0               # else ==> return zero
+        j       exit_fib
 
-sub $a0, $a0, 1     # n-1
-jal fib             # fib(n-1)
-add $a0, $a0, 1
+# if (n == 1) then return 1
+B2_fib:
+        seq     $t0, $a0, 1
+        beq     $t0, $zero, B3_fib   # if (n != 1) ==> goto B3
+        li      $v0, 1               # else ==> return 1
+        j       exit_fib
 
-lw $ra, 0($sp)      # restoring return address
-add $sp, $sp, 4
+# else return fib(n-1) + fib(n-2);
+B3_fib:
+        subu    $s0, $a0, 1          # Save n - 1 in s0
+        move    $a0, $s0             # arg1 = n -1
+        jal     fib     
+        
+        subu    $a0, $s0, 1          # arg1 = n -2
+        move    $s0, $v0             # Save fib(n-1)
+        jal     fib
+        add     $v0, $v0, $s0        # Return fib(n-1) + fib(n-2)
+        
+        
+exit_fib:
+        lw      $ra, 20($sp)         # Restore return address
+        lw      $fp, 16($sp)         # Restore frame pointer
+        lw      $s0, 12($sp)         # Restore s0
+        addiu   $sp, $sp, 32         # Pop stack frame
+        
+        jr      $ra                  # Return to caller    
+                
 
+# a, b = two previos Fib numbers, n = Fib number to generate
+# Must be called: Fib(1, 0, n)
+#
+#int fib_iter(int a, int b, int count)
+#{
+#       if (count == 1)
+#               return b;
+#       else
+#               return fib_iter(a + b, a, count - 1);
+#}
+#
+# a0 = a, a1 = b, a2 = count
 
-sub $sp, $sp, 4     # Push return value
-sw $v0, 0($sp)
+fib_iter:
 
-# Calling fib(n-2)
-sub $sp, $sp, 4     # storing return address
-sw $ra, 0($sp)
+# if (count == 1) then return b
 
-sub $a0, $a0, 2     # n = n-2
-jal fib             # call fib(n-2)
-add $a0, $a0, 2
+start_fib_iter:
+        seq     $t0, $a2, $zero
+        beq     $t0, $zero, B1_fib_iter # if (count != 0) ==> goto B1
+        move    $v0, $a1
+        j       exit_fib_iter
 
-lw $ra, 0($sp)      # restoring return address
-add $sp, $sp, 4
-# ---------------
-lw $s7, 0($sp)      # Pop return value
-add $sp, $sp, 4
+# else return fiber_iter(a + b, a, count - 1)
+B1_fib_iter:
+        add     $t0, $a0, $a1          # t0 = a + b
+        move    $a1, $a0               # arg2 = a
+        move    $a0, $t0               # arg1 = a + b
+        subu    $a2, $a2, 1            # arg3 = count - 1
+        j       start_fib_iter
 
-add $v0, $v0, $s7   # f(n - 2)+fib(n-1)
-
-jr $ra              # decrement/next in stack
-
-zero:
-li $v0, 0
-jr $ra
-
-one:
-li $v0, 1
-jr $ra
-
-.data
-prompt: .asciiz "The code is based on Adel Zare's answer at <a href='https://stackoverflow.com/questions/22976456'>StackOverflow</a>\nThis program calculates Fibonacci sequence from 1 to n.\nPlease enter a non-negative number n:\n"
-result: .asciiz "F_"
-result2: .asciiz " = "
-endl: .asciiz "\n"
+exit_fib_iter:
+        jr      $ra
